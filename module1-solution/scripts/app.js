cfc34a02ab4c1c2bfe937811a915a9e9d546b5cc
Leonardo Mello <leo@leonardomello.com>
2016-09-17 01:49:08 -0300

    Colors working

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -1,12 +1,41 @@
 (function(){
 'use strict'
 
-angular.module('myFirstApp', [])
-.controller('MyFirstController', function($scope){
-  $scope.name='Leo';
-  $scope.sayHelo = function(){
-    return 'Hi mother fucker!'
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.dishesStr='';
+  $scope.dishesCount=null;
+  $scope.message='';
+
+  $scope.checkIfTooMuch = function(){
+    $scope.dishesCount = countNotEmpty(transformInArray($scope.dishes, ","));
+    if ($scope.dishesCount===0){
+      $scope.message = 'Please enter data first!';
+    } else if ($scope.dishesCount > 0  && $scope.dishesCount <= 3){
+      $scope.message = 'Enjoy!';
+    } else {
+      $scope.message = 'Too much!';
+    }
   };
-});
+}
+
+function transformInArray(someString, separator){
+  var str = (someString ? someString : "");
+  return str.split(separator);
+}
+
+function countNotEmpty(array){
+  var dCount = 0;
+  angular.forEach(array, function (value){
+    if ((value !== null && value.trim().length > 0)){
+      dCount++;
+    }
+  });
+  return dCount;
+}
+
 
 })();
