61aed3a996cf14063379796f1a8f58a2e31665ad
Leonardo Mello <leo@leonardomello.com>
2017-10-08 21:13:02 -0300

    fixed sign up controller

--- module5-solution/src/public/myinfo/myinfo.controller.js
+++ module5-solution/src/public/myinfo/myinfo.controller.js
@@ -9,10 +9,11 @@ MyinfoController.$inject = ['MenuService', '$scope', 'ApiPath'];
 function MyinfoController(MenuService, $scope, ApiPath){
   var $ctrl = this;
   var $srvc = MenuService;
-  $scope.sucess=false;
   $scope.user = $srvc.userSignup;
+  $scope.signedUp=!angular.equals($scope.user, {});
   $scope.basePath = ApiPath;
   console.log($scope.user);
+  console.log($scope.signedUp);
 }
 
 })();
